caa5a45c29846bb9543cd8476a75b34e1663d8a8
Djordje Ljubinkovic <djordje.ljubinkovic@lotusflare.com>
2020-02-08 12:35:23 +0100

    Initial Commit

---
+++ EthereumSigner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EthereumSigner
+//
+//  Created by Djordje Ljubinkovic on 08/02/2020.
+//  Copyright © 2020 Djordje Ljubinkovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
